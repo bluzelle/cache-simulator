4bc96c769fdea3110632470d4d25ae4c96c5e022
isabel@lotus <isabel@izabyl.com>
2019-05-30 13:37:20 -0700

    first draft of kademlia routing table

--- kcache/kademlia_routing_table.cpp
+++ kcache/kademlia_routing_table.cpp
@@ -1,1 +1,74 @@
 #include <kcache/kademlia_routing_table.hpp>
+#include <algorithm>
+#include <vector>
+
+using namespace ksim::kcache;
+
+kademlia_routing_table::kademlia_routing_table(ksim::kcache::node_id_t id, unsigned int bucket_size)
+    : my_id(id)
+    , bucket_size(bucket_size)
+{
+}
+
+void kademlia_routing_table::insert(ksim::kcache::node_id_t kid, ksim::actor_id_t id, unsigned int latency)
+{
+    peer_record_t peer = std::make_pair(kid, id);
+
+    if (kid == this->my_id || this->peers.count(peer) > 0)
+    {
+        return;
+    }
+
+    unsigned long long dist = this->my_id ^ kid; // Kademlia distance metric
+    auto bucket = __builtin_clzll(dist); // equiv to "auto bucket = (8*sizeof(node_id_t)) - floor(log2(dist));"
+
+    this->buckets[bucket].emplace(latency, peer);
+    this->peers.emplace(peer);
+
+    if (this->buckets[bucket].size() > this->bucket_size)
+    {
+        this->peers.erase((*(this->buckets[bucket].rbegin())).second);
+        this->buckets[bucket].erase(*(this->buckets[bucket].rbegin()));
+    }
+}
+
+std::list<kademlia_routing_table::peer_record_t> kademlia_routing_table::k_nearest(ksim::kcache::node_id_t target)
+{
+    if(this->peers.size() <= this->bucket_size)
+    {
+        std::list<peer_record_t> result(this->peers.begin(), this->peers.end());
+        return result;
+    }
+
+    std::vector<peer_record_t> peers_list(this->peers.begin(), this->peers.end());
+
+    auto ctor = [target](const peer_record_t& a, const peer_record_t& b)
+    {
+        return (target ^ a.first) < (target ^ b.first);
+    };
+
+    // sort the list just enough that the first r elements are the k-closest r elements in the list
+    std::nth_element(peers_list.begin(), peers_list.begin() + (this->bucket_size - 1), peers_list.end(), ctor);
+
+    std::list<peer_record_t> result(peers_list.begin(), peers_list.begin() + (this->bucket_size - 1));
+    return result;
+}
+
+std::list<kademlia_routing_table::peer_record_t> kademlia_routing_table::random()
+{
+    if(this->peers.size() <= this->bucket_size)
+    {
+        std::list<peer_record_t> result(this->peers.begin(), this->peers.end());
+        return result;
+    }
+
+    std::vector<std::pair<unsigned int, peer_record_t>> peers_list;
+    std::transform(this->peers.begin(), this->peers.end(), peers_list.begin(),
+            [this](const auto& peer){return std::make_pair(this->rand.next_uint(), peer);});
+    std::nth_element(peers_list.begin(), peers_list.begin() + (this->bucket_size - 1), peers_list.end());
+
+    std::list<peer_record_t> result;
+    std::transform(peers_list.begin(), peers_list.begin() + (this->bucket_size - 1), result.begin(), [](const auto& pair){return pair.second;});
+
+    return result;
+}
--- kcache/kademlia_routing_table.hpp
+++ kcache/kademlia_routing_table.hpp
@@ -1,15 +1,35 @@
 #pragma once
 #include <kcache/kademlia_types.hpp>
+#include <include/types.hpp>
+#include <list>
+#include <unordered_map>
+#include <set>
+#include <random/random.hpp>
 
 namespace ksim::kcache
 {
     class kademlia_routing_table
     {
     public:
-        kademlia_routing_table(node_id_t id);
+        using peer_record_t = std::pair<node_id_t, ksim::actor_id_t>;
+
+        kademlia_routing_table(node_id_t id, unsigned int bucket_size);
+
+        void insert(node_id_t kid, ksim::actor_id_t, unsigned int latency);
+
+        std::list<peer_record_t> k_nearest(node_id_t target);
+        std::list<peer_record_t> random();
 
     private:
+
+        std::set<peer_record_t> peers;
+
+        using bucket_t = std::set<std::pair<unsigned int, peer_record_t>>; // ordered by latency
+        std::unordered_map<unsigned int, bucket_t> buckets;
+
+        ksim::random rand;
         node_id_t my_id;
+        unsigned int bucket_size;
     };
 
 }
